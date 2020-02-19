297ef9f64a12e69cbae0bb45bc9f302b6c62710a
Kevin McCarthy <km.mccarthy11@gmail.com>
2020-02-19 14:18:00 -0500

    add iniial footer structure

---
+++ logo-game/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+  
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>Memory React Game © 2020 Copyright:&nbsp;<a href="https://github.com/kmccarthy1" target="_blank" rel="noopener noreferrer">Kevin McCarthy</a></span>
+  </footer>
+);
+
+export default Footer;
